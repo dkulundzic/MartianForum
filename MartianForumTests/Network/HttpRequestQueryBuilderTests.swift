d7a64d7dd3956effe678f5e293cbb1bc1c5d0170
Domagoj Kulundzic <domagoj.kulundzic.business@gmail.com>
2018-05-31 22:20:43 +0200

    Added a Unit Test file for the HttpRequestQueryBuilder.

---
+++ MartianForumTests/Network/HttpRequestQueryBuilderTests.swift
@@ -0,0 +1,13 @@
+//
+//  HttpRequestQueryBuilderTests.swift
+//  MartianForumTests
+//
+//  Created by Domagoj Kulundzic on 31/05/2018.
+//  Copyright © 2018 Codeopolis. All rights reserved.
+//
+
+import XCTest
+
+class HttpRequestQueryBuilderTests: XCTestCase {
+  // TODO: - Add tests
+}
